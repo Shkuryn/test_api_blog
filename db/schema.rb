e614ef90f64ee771350acee0a0a8014e35065915
Igor <shkuryn@gmail.com>
2022-02-19 19:45:59 +0300

    add article model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_19_103436) do
+ActiveRecord::Schema.define(version: 2022_02_19_163637) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "header"
+    t.text "body"
+    t.integer "user_id"
+    t.string "category"
+  end
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
